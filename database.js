d559c16f4f8da38b8c8df704e8d4aa0ebfdde017
Chase Aspengren <caspengren17@gmail.com>
2025-02-24 21:50:07 -0600

    connected to firebase

--- database.js
+++ database.js
@@ -1,24 +1,25 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBcR57SNi9wF2FIPciPxFU1IWPsEf5vx0g",
-    authDomain: "bdplsite.firebaseapp.com",
-    databaseURL: "https://bdplsite-default-rtdb.firebaseio.com",
-    projectId: "bdplsite",
-    storageBucket: "bdplsite.firebasestorage.app",
-    messagingSenderId: "459708893175",
-    appId: "1:459708893175:web:f99483c6105308274d50f2",
-    measurementId: "G-MDYZ8EQVL9"
-  };
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// firebase.js
+
+// Import Firebase libraries (using CDN)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcR57SNi9wF2FIPciPxFU1IWPsEf5vx0g",
+  authDomain: "bdplsite.firebaseapp.com",
+  databaseURL: "https://bdplsite-default-rtdb.firebaseio.com",
+  projectId: "bdplsite",
+  storageBucket: "bdplsite.firebasestorage.app",
+  messagingSenderId: "459708893175",
+  appId: "1:459708893175:web:f99483c6105308274d50f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+// Export Firebase functions for use in other files
+export { database, ref, set, get, child };
